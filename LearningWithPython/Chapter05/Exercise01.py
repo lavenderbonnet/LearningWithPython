13c978dd188c62bce919312dceb3ddc71257b671
lavenderbonnet <lavenderbonnet@gmail.com>
2021-08-03 12:31:11 -0500

    conditionals

---
+++ LearningWithPython/Chapter05/Exercise01.py
@@ -0,0 +1,30 @@
+# --------------------------------------------------------------------------
+# Author : Lilac Walia
+# Date : 8/2/2021
+# --------------------------------------------------------------------------
+
+n = int(input("Choose a number 0 - 6 "))
+
+if n == 0:
+    print("It's Sunday!")
+
+elif n == 1:
+    print("It's Monday!")
+
+elif n == 2:
+    print("It's Tuesday!")
+
+elif n == 3:
+    print("It's Wednesday!")
+
+elif n == 4:
+    print("It's Thursday!")
+
+elif n == 5:
+    print("It's Friday!")
+
+elif n == 6:
+    print("It's Saturday!")
+
+else:
+    print("I said 0 - 6.")
