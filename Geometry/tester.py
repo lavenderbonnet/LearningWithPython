2dc47c62611ff4ab1f5e94d3dc8cb142ce99840b
lavenderbonnet <lavenderbonnet@gmail.com>
2021-11-07 12:01:26 -0600

    points and lines

---
+++ Geometry/tester.py
@@ -0,0 +1,15 @@
+# --------------------------------------------------------------------------
+# Author : Lilac Walia
+# Date : 11/7/2021
+# --------------------------------------------------------------------------
+
+from point import Point
+
+class Tester:
+    
+    def test_points(self):
+        p1 = Point(0, 0)
+        print(p1)
+
+    def run_tests(self):
+        self.test_points()
