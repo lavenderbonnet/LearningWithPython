3ec0657b701f825392a7bd6fce0f81ddcd435e27
lavenderbonnet <lavenderbonnet@gmail.com>
2021-11-29 22:19:17 -0600

    randomization

--- LearningWithPython/Chapter12/random.py
+++ LearningWithPython/Chapter12/random.py
@@ -8,26 +8,26 @@ import random
 rng = random.Random(3)
 # rng stands for random number generator
 
-# for n in range(1,100):
-#     dice_throw = rng.randrange(1, 7) # doesn't actually include 7
-#     delay_in_seconds = rng.random() # * 5.0 [0,1)
+for n in range(1,100):
+    dice_throw = rng.randrange(1, 7) # doesn't actually include 7
+    delay_in_seconds = rng.random() # * 5.0 [0,1)
 
-#     # print(dice_throw)
-#     print(delay_in_seconds)
+    # print(dice_throw)
+    print(delay_in_seconds)
 
-# n = 1000001
-# histogram = [0] * 10
-# for i in range(1,n):
+n = 1000001
+histogram = [0] * 10
+for i in range(1,n):
     # uniform distribution
-    # v = rng.randrange(100)
-    # histogram[v//10] = histogram[v//10] + 1
+    v = rng.randrange(100)
+    histogram[v//10] = histogram[v//10] + 1
 
     # bell curve distribution
-    # v = int(rng.normalvariate(50,10))
-    # histogram[v//10] = histogram[v//10] + 1
-    # print(v)
+    v = int(rng.normalvariate(50,10))
+    histogram[v//10] = histogram[v//10] + 1
+    print(v)
 
-# print(histogram)
+print(histogram)
 
 
 # SHUFFLE CARDS
@@ -38,7 +38,7 @@ for c in colors:
   for i in range(1,14):
       cards.append((c,i))
 
-# print(cards)
+print(cards)
 # shuffle the cards
 rng.shuffle(cards)
 print(cards[:10])
