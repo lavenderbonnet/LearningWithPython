4bdd5ef952d099166db5e34435fc9ca2257b916a
lavenderbonnet <lavenderbonnet@gmail.com>
2021-12-04 16:28:06 -0600

    reversing a file's lines

--- LearningWithPython/Chapter13/reversedOrder.py
+++ LearningWithPython/Chapter13/reversedOrder.py
@@ -8,4 +8,15 @@ import os
 os.chdir("D:\Lilac\Coding\Python\LearningWithPython\Chapter13")
 cwd = os.getcwd()
 
-read_file = ("13.5toRead.txt", "r")
+o_f = open("reverseOriginalFile.txt", "r")
+c = o_f.readlines()
+# print(c)
+o_f.close()
+
+c.reverse()
+# print(c)
+
+ro_f = open("reversedFile.txt", "w")
+for s in c:
+    ro_f.write(s)
+ro_f.close()
