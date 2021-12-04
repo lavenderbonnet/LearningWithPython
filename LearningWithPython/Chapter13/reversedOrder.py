18ec3cfdfa88b3fdc0ab705030068ed964b10308
lavenderbonnet <lavenderbonnet@gmail.com>
2021-12-04 15:47:47 -0600

    regrouping

---
+++ LearningWithPython/Chapter13/reversedOrder.py
@@ -0,0 +1,11 @@
+# --------------------------------------------------------------------------
+# Author : Lilac Walia
+# Date : 12/4/2021
+# --------------------------------------------------------------------------
+
+import os
+
+os.chdir("D:\Lilac\Coding\Python\LearningWithPython\Chapter13")
+cwd = os.getcwd()
+
+read_file = ("13.5toRead.txt", "r")
