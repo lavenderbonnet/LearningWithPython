23a44a556a0ada13e10d7d8bb003bf057085d318
lavenderbonnet <lavenderbonnet@gmail.com>
2022-01-19 22:48:16 -0600

    adding in the queens/beach balls (error)

--- LearningWithPython/Chapter17/03drawQueens.py
+++ LearningWithPython/Chapter17/03drawQueens.py
@@ -11,31 +11,41 @@ def draw_board(board):
     pygame.init
     colors = [(255, 0, 0), (0, 0, 0)]
 
-    # n = len(board)
-    square_size = 480 // board
-    surface_size = board*square_size
+    n = len(board)
+    square_size = 480 // n
+    surface_size = n*square_size
 
     surface = pygame.display.set_mode((surface_size, surface_size))
 
-    for row in range(board):
-            color_index = row % 2
-            for column in range(board):
-                square = (column*square_size, row*square_size, square_size, square_size)
-                surface.fill(colors[color_index], square)
-                color_index = (color_index + 1) % 2
+    ball_file_path = "D:\Lilac\Coding\Python\LearningWithPython\Chapter17\\beachBall.png"
+    ball = pygame.image.load(ball_file_path)
+    ball_offset = (square_size-ball.get_width()) // 2
 
     while True:
         ev = pygame.event.poll()
         if ev.type == pygame.QUIT:
             break
 
+        for row in range(n):
+            color_index = row % 2
+            for column in range(n):
+                square = (column*square_size, row*square_size, square_size, square_size)
+                surface.fill(colors[color_index], square)
+                color_index = (color_index + 1) % 2
+            
+        for (column, row) in enumerate(board):
+            surface.blit(ball, (column*square_size + ball_offset, row*square_size) + ball_offset)
+            
         pygame.display.flip()
 
     pygame.quit
 
 
 def main():
-    draw_board(12)
+    draw_board([0, 5, 3, 1, 6, 4, 2])
+    draw_board([6, 4, 2, 0, 5, 7, 1, 3])
+    draw_board([9, 6, 0, 3, 10, 7, 2, 4, 12, 8, 11, 5, 1])
+    draw_board([11, 4, 8, 12, 2, 7, 3, 15, 0, 14, 10, 6, 13, 1, 5, 9])
 
 if __name__ == "__main__":
     main()
