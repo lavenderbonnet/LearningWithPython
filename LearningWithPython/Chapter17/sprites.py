ac6c553c7b0484d34b183624d3082dd02a5e5b08
lavenderbonnet <lavenderbonnet@gmail.com>
2022-01-28 17:53:59 -0600

    update

--- LearningWithPython/Chapter17/sprites.py
+++ LearningWithPython/Chapter17/sprites.py
@@ -8,15 +8,29 @@ import pygame
 import random
 
 # ---------------------------------------------------------------------------
+gravity = 0.0001
+
 class QueenSprite:
 
     def __init__(self, img, target_posn):
         self.image = img
         self.target_posn = target_posn
-        self.posn - target_posn
+        (x, y) = target_posn
+        self.posn = (x, 0)
+        self.y_velocity = 0
     
     def update(self):
-        return
+        self.y_velocity += gravity
+        (x, y) = self.posn
+        new_y_pos = y + self.y_velocity
+        (target_x, target_y) = self.target_posn
+        dist_to_go = target_y - new_y_pos
+
+        if dist_to_go < 0:
+            self.y_velocity = -0.65 * self.y_velocity
+            new_y_pos = target_y + dist_to_go
+        
+        self.posn = (x, new_y_pos)
 
     def draw(self, target_surface):
         target_surface.blit(self.image, self.posn)
@@ -38,10 +52,12 @@ def draw_board(board):
     ball = pygame.image.load(ball_file_path)
     ball_offset = (square_size-ball.get_width()) // 2
 
+    # queens list
     all_sprites = []
-
+    # add the queens to the queen list
     for (column, row) in enumerate(board):
-        a_queen = QueenSprite(ball, (column*square_size+ball_offset, row*square_size+ball_offset))
+        a_queen = QueenSprite(ball, (column*square_size+ball_offset, \
+                                        row*square_size+ball_offset))
         all_sprites.append(a_queen)
 
     while True:
@@ -58,9 +74,10 @@ def draw_board(board):
                 square = (column*square_size, row*square_size, square_size, square_size)
                 surface.fill(colors[color_index], square)
                 color_index = (color_index + 1) % 2
-            
-        for (column, row) in enumerate(board):
-            surface.blit(ball, (column*square_size + ball_offset, row*square_size + ball_offset))
+        
+        # preset queens
+        # for (column, row) in enumerate(board):
+        #     surface.blit(ball, (column*square_size + ball_offset, row*square_size + ball_offset))
             
         for sprite in all_sprites:
             sprite.draw(surface)
@@ -72,9 +89,10 @@ def draw_board(board):
 
 def main():
     draw_board([0, 5, 3, 1, 6, 4, 2])
-    draw_board([6, 4, 2, 0, 5, 7, 1, 3])
-    draw_board([9, 6, 0, 3, 10, 7, 2, 4, 12, 8, 11, 5, 1])
-    draw_board([11, 4, 8, 12, 2, 7, 3, 15, 0, 14, 10, 6, 13, 1, 5, 9])
+    # draw_board([0, 5, 3, 1, 6, 4, 2])
+    # draw_board([6, 4, 2, 0, 5, 7, 1, 3])
+    # draw_board([9, 6, 0, 3, 10, 7, 2, 4, 12, 8, 11, 5, 1])
+    # draw_board([11, 4, 8, 12, 2, 7, 3, 15, 0, 14, 10, 6, 13, 1, 5, 9])
 
 if __name__ == "__main__":
     main()
